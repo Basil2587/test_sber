da78c44339761e39265223a7aa6cb7e0974d3cfa
Basil <vogurt007@gmail.com>
2021-05-06 15:14:11 +0300

    full_70%

--- parser.py
+++ parser.py
@@ -4,6 +4,25 @@ import pandas as pd
 from urllib.parse import quote
 
 
+def get_html(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.text
+        print(r.text)
+    if r.status_code == 404:
+        print('Страница не существует!')
+
+
+def get_link(html):
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        list2 = [p.text.strip() for p in soup.find_all('td')
+            if p.text is not None]
+    except Exception.DoesNotExist:
+        list2 = ''
+    return list2
+
+
 def parse_table(table):
     res = pd.DataFrame()
     id = 0
@@ -13,8 +32,6 @@ def parse_table(table):
     status = ''
     type = ''
     reg_num = ''
-    date_reg = ''
-    date_close = ''
 
     data_tr = table.find('tr', {'class': 'sro-link'})
     name_org = data_tr.find_all('td')[1].text.replace('<br />', '\n').strip()
@@ -24,48 +41,57 @@ def parse_table(table):
     type = data_tr.find_all('td')[5].text.replace('<br />', '\n').strip()
     reg_num = data_tr.find_all('td')[6].text.replace('<br />', '\n').strip()
 
-    date_reg = list2[6]
-    date_close_td = list2[7]
-    if date_close_td is not None:
-        date_close = date_close_td
-
     res = res.append(pd.DataFrame([[
         id, name_org, inn, ogrn, status, type,
-        reg_num, date_reg, date_close]],
+        reg_num]],
         columns=[
         'id', 'name_org', 'inn', 'ogrn', 'status',
-        'type', 'reg_num', 'date_reg', 'date_close']), ignore_index=True)
+        'type', 'reg_num']), ignore_index=True)
     print(res)
     return(res)
 
 
-if __name__ == '__main__':
+def parse_table2(table2):
+    res = pd.DataFrame()
+    date_reg = ''
+    date_close = ''
+    date_reg = table2[6]
+    date_close_td = table2[7]
+    if date_close_td is not None:
+        date_close = date_close_td
+
+    res2 = res.append(pd.DataFrame([[
+         date_reg, date_close]],
+        columns=[
+         'date_reg', 'date_close']), ignore_index=True)
+    print(res2)
+    return(res2)
+
+
+def main():
     print("\n---------\n")
     num_inn = input("Введите ваш ИНН: ")
     # + '0278128540'
     print("\n---------\n")
     url_inn = '/reestr?m.fulldescription=&m.shortdescription=&m.inn='
     url = 'http://reestr.nostroy.ru'
-    url_1 = url+(url_inn)+quote(num_inn)
-    r = requests.get(url_1)
-    with open('page1.html', 'w') as output_file:
-        output_file.write(r.text)
-    soup = BeautifulSoup(r.text, 'lxml')
+    url_inn = url+(url_inn)+quote(num_inn)
+    soup = BeautifulSoup(get_html(url_inn), 'lxml')
     tables = soup.find_all('table', {'class': 'items'})
     rel_cl = [tag.get('rel')for tag in soup.find_all('tr')
                 if not tag.get('rel')is None]
     num_clienta = rel_cl[0]
-    url_2 = url + num_clienta
-    t = requests.get(url_2)
-    with open('page2.html', 'w') as output_file:
-        output_file.write(t.text)
-    soup2 = BeautifulSoup(t.text, 'lxml')
-    tables2 = soup2.find_all('td')
-    list2 = [p.text.strip() for p in soup2.find_all('td')if p.text is not None]
-
+    url_client = url + num_clienta
+    html = get_html(url_client)
+    table2 = get_link(html)
     result = pd.DataFrame()
     for item in tables:
         res = parse_table(item)
         result = result.append(res)
-
+    data_date = parse_table2(table2)
+    result = result.append(data_date)
     result.to_excel('result.xlsx')
+
+
+if __name__ == '__main__':
+    main()
