a7160e416144b144f91a682f7d1b28a1c5ce5556
Basil <vogurt007@gmail.com>
2021-05-24 12:47:12 +0300

    full_91

--- parser.py
+++ parser.py
@@ -16,8 +16,8 @@ def get_html(url):
 def get_date(html):
     soup = BeautifulSoup(html, 'lxml')
     try:
-        data_date = [p.text.strip() for p in soup.find_all('td')
-            if p.text is not None]
+        data_date = [p.text.strip()
+                     for p in soup.find_all('td')if p.text is not None]
     except Exception.DoesNotExist:
         data_date = ''
     return data_date
@@ -27,7 +27,7 @@ def find_archive(page_archive):
     soup = BeautifulSoup(page_archive, 'lxml')
     try:
         data_arc = [p.text.replace('&downarrow;', '\n').strip()
-                for p in soup.find_all('td') if p.text is not None]
+                    for p in soup.find_all('td') if p.text is not None]
     except Exception.DoesNotExist:
         data_arc = ''
     return data_arc
@@ -37,8 +37,8 @@ def find_right(page_right):
     soup = BeautifulSoup(page_right, 'lxml')
     try:
         data_right = [p.text.replace('&downarrow;', '\n').strip()
-                for p in soup.find_all('table', {'class': 'items'})
-                    if p.text is not None]
+                      for p in soup.find_all('table', {'class': 'items'})
+                      if p.text is not None]
     except Exception.DoesNotExist:
         data_right = ''
     return data_right
@@ -82,7 +82,8 @@ def parse_page_2(page_2):
     if date_close_td is not None:
         date_close = date_close_td
 
-    res = res.append(pd.DataFrame([[date_reg, date_close]],
+    res = res.append(pd.DataFrame(
+        [[date_reg, date_close]],
         columns=['date_reg', 'date_close']), ignore_index=True)
     return res
 
@@ -116,44 +117,42 @@ def parse_page_right(page_right):
     inform_right = page_right[0]
     if inform_right is not None:
         inform_of_the_right = inform_right
-    res = res.append(pd.DataFrame([[inform_of_the_right]],
-        columns=['inform_of_the_right']), ignore_index=True)
+    res = res.append(
+        pd.DataFrame([[inform_of_the_right]], columns=['inform_of_the_right']))
     return res
 
 
 def main():
-    print("\n---------\n")
-    num_inn = input("Введите ваш номер ИНН или список через пробел: ").split()
-    # + 0278128540 770965012811
-    return num_inn
+    for i in num_inn:
+        url_inn = base_url+(rel_inn)+i
+        soup = BeautifulSoup(get_html(url_inn), 'lxml')
+        tables = soup.find_all('table', {'class': 'items'})
+        rel_cl = [tag.get('rel')for tag in soup.find_all('tr')
+                  if not tag.get('rel') is None]
+        num_clienta = rel_cl[0]
+        url_client = base_url + num_clienta
+        html = get_html(url_client)
+        url_arch = url_client + '/certificates'
+        url_of_the_right = url_client + '/rights'
+        page_get_right = get_html(url_of_the_right)
+        page_archive = get_html(url_arch)
+        for item in tables:
+            res = parse_table(item)
+        data_date = parse_page_2(get_date(html))
+        archive = parse_page_arch(find_archive(page_archive))
+        the_right = parse_page_right(find_right(page_get_right))
+        result = pd.DataFrame()
+        df = pd.concat([res, data_date, archive, the_right], axis=1)
+        result = result.append(df, ignore_index=True)
+        engine = create_engine('sqlite:///pars_data.db')
+        result.to_sql('users', con=engine, if_exists='append')
 
 
 if __name__ == '__main__':
     base_url = 'http://reestr.nostroy.ru'
     rel_inn = '/reestr?m.fulldescription=&m.shortdescription=&m.inn='
-    for i in main():
-        url_inn = base_url+(rel_inn)+i
-        soup = BeautifulSoup(get_html(url_inn), 'lxml')
-        tables = soup.find_all('table', {'class': 'items'})
-        rel_cl = [tag.get('rel')for tag in soup.find_all('tr')
-                if not tag.get('rel')is None]
-        num_clienta = rel_cl[0]
-        url_client = base_url + num_clienta
-        html = get_html(url_client)
-        page_2 = get_date(html)
-        url_arch = url_client + '/certificates'
-        url_of_the_right = url_client + '/rights'
-        page_get_right = get_html(url_of_the_right)
-        page_archive = get_html(url_arch)
-        page_arch = find_archive(page_archive)
-        page_right = find_right(page_get_right)
-        result = pd.DataFrame()
-        for item in tables:
-            res = parse_table(item)
-        data_date = parse_page_2(page_2)
-        archive = parse_page_arch(page_arch)
-        the_right = parse_page_right(page_right)
-        df = pd.concat([res, data_date, archive, the_right], axis=1)
-        result = result.append(df, ignore_index=True)
-        engine = create_engine('sqlite:///pars_data.db')
-        result.to_sql('users', con=engine, if_exists='append')
+    print("\n---------\n")
+    num_inn = input("Введите ваш номер ИНН или список через пробел: ").split()
+    # 0278128540 770965012811
+    main()
+    print("\n---------\n")
